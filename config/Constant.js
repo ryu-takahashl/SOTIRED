673ad7ebe6bfddb8df77ee4fe8755f99c3b6d179
ryutakahashi <r.takahashi@soushin-lab.co.jp>
2020-10-25 20:02:58 +0900

    [add]cloud translation と cloud vision の post 先を定数として管理

---
+++ config/Constant.js
@@ -0,0 +1,6 @@
+const constant = {
+  CLOUD_TRANSLATION_URL: 'https://translation.googleapis.com/language/translate/v3',
+  CLOUD_VISION_URL: 'https://vision.googleapis.com/v1/images:annotate?key=',
+};
+
+export default constant;
